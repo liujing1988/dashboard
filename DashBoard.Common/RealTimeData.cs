fe43e97e8e8231619a5fed70cf9cc54bb2ed7a42
liuj <976579213@qq.com>
2015-12-01 11:01:48 +0800

    20151201

--- DashBoard.Common/RealTimeData.cs
+++ DashBoard.Common/RealTimeData.cs
@@ -6,11 +6,36 @@ using System.Threading.Tasks;
 
 namespace Dashboard.Common
 {
+    /// <summary>
+    /// 交易量参数传递
+    /// 作者：刘静
+    /// 修改日期：2015-11-30
+    /// </summary>
     public class RealTimeData
     {
+        /// <summary>
+        /// 时间，精确到分钟
+        /// </summary>
         public string Minute { get; set; }
+
+        /// <summary>
+        /// 日期
+        /// </summary>
+        public string Day { get; set; }
+
+        /// <summary>
+        /// 交易量
+        /// </summary>
         public decimal TradeAmount { get; set; }
+
+        /// <summary>
+        /// 查询开始日期
+        /// </summary>
         public string beginDate { get; set; }
+
+        /// <summary>
+        /// 查询结束日期
+        /// </summary>
         public string endDate { get; set; }
     }
 }
--- DashBoard.Common/TradeDayVolume.cs
+++ DashBoard.Common/TradeDayVolume.cs
@@ -7,18 +7,39 @@ namespace DashBoard.Common
 {
     public class TradeDayVolume
     {
-        //委托笔数
+        //日委托笔数
         public int NumOrder { get; set; }
+
+        //日交易量阈值
+        public int ThDayNumOrder { get; set; }
+
+        //分钟交易量阈值
+        public int ThMiNumOrder { get; set; }
+
+        //秒交易量阈值
+        public int ThSeNumOrder { get; set; }
+
+        //分钟委托笔数
+        public int MiNumOrder { get; set; }
+
+        //秒委托笔数
+        public int SeNumOrder { get; set; }
+
         //成交笔数
         public int NumVolum { get; set; }
+
         //撤单笔数
         public int NumRevoke { get; set; }
+
         //成交金额
         public string VolumAmt { get; set; }
+
         //逆回购金额
         public string RRPAmt { get; set; }
+
         //买入金额
         public string BuyAmt { get; set; }
+
         //卖出金额
         public string SalesAmt { get; set; }
     }
